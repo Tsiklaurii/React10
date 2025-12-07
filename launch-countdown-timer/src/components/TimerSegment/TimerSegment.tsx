8e65739059e12299e9cab5cf92ea828f4cc97cd1
tsiklaurii <tsiklaurinino321@gmail.com>
2025-12-07 14:02:19 +0400

    TimerSegment styles

--- launch-countdown-timer/src/components/TimerSegment/TimerSegment.tsx
+++ launch-countdown-timer/src/components/TimerSegment/TimerSegment.tsx
@@ -18,9 +18,13 @@ const TimerSegment = ({ value, label }: TimerSegmentProps) => {
         <div className="flex flex-col items-center">
             <div className="w-[70px] h-[66.26px] sm:w-[148px] sm:h-[140px] relative flex items-center justify-center bg-[#343650] text-[#FB5E84] text-[36px] sm:text-[80px] font-bold rounded-lg shadow-[0_10px_#191A23] overflow-hidden">
                 <div className="w-[148px] h-[33px] sm:h-[70px] bg-[#000000] opacity-25 absolute top-0 rounded-t-lg"></div>
-                <div className="w-1.5 sm:w-3 h-1.5 sm:h-3 bg-[#191A23] rounded-2xl absolute left-[67px] sm:-left-1.5"></div>
+
+                <div className="w-1.5 sm:w-3 h-1.5 sm:h-3 bg-[#191A23] rounded-2xl absolute left-[68px] sm:-left-1.5"></div>
                 <div className="w-1.5 sm:w-3 h-1.5 sm:h-3 bg-[#191A23] rounded-2xl absolute right-[68px] sm:-right-1.5"></div>
-                {format(current)}
+
+                <div className="mb-[3px] sm:mb-2.5">
+                    {format(current)}
+                </div>
             </div>
 
             <p className="text-[#8385A9] text-[7px] sm:text-[14px] tracking-[5.92px] font-bold mt-4">{label}</p>
